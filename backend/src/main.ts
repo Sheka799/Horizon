2a6c37232bdf85e173d9c6f9bbf1d534094ea807
Evgeny Krivolapov <sheka1991_91@mail.ru>
2026-03-22 11:46:14 +0300

    Настройка redis

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,15 +3,46 @@ import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import * as cookieParser from 'cookie-parser'
 import { ValidationPipe } from '@nestjs/common'
+import IORedis from 'ioredis'
+import * as session from 'express-session'
+import { parseBoolean } from './libs/common/utils/parse-boolean.util'
+import * as ms from 'ms'
+import { RedisStore } from 'connect-redis'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const config = app.get(ConfigService)
+	const redis = new IORedis(config.getOrThrow<string>('REDIS_URL'))
 
 	app.use(cookieParser(config.getOrThrow<string>('COOKIE_SECRET')))
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true
+		})
+	)
+
+	app.use(
+		session({
+			secret: config.getOrThrow<string>('SESSION_SECRET'),
+			name: config.getOrThrow<string>('SESSION_NAME'),
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+				httpOnly: parseBoolean(
+					config.getOrThrow<string>('SESSION_HTTP_ONLY')
+				),
+				secure: parseBoolean(
+					config.getOrThrow<string>('SESSION_SECURE')
+				),
+				sameSite: 'lax',
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+				maxAge: ms(config.getOrThrow<string>('SESSION_MAX_AGE')),
+			},
+			store: new RedisStore({
+				client: redis,
+				prefix: config.getOrThrow<string>('SESSION_FOLDER')
+			})
 		})
 	)
 
