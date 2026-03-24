d55517c484ad81a63eab704102e0e9724c7cb81d
Evgeny Krivolapov <sheka1991_91@mail.ru>
2026-03-24 20:26:30 +0300

    Настройка провайдеров и переключение темы

---
+++ frontend/src/shared/components/ui/index.ts
@@ -0,0 +1,5 @@
+export * from './Button'
+export * from './Card'
+export * from './DropdownMenu'
+export * from './Sonner'
+export * from './ToggleTheme'
---
+++ frontend/src/shared/providers/MainProvider.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { type PropsWithChildren } from "react";
+import { TanstackQueryProvider } from "./TanstackQueryProvider";
+import { ThemeProvider } from "./ThemeProvider";
+import { ToastProvider } from "./ToastProvider";
+
+export function MainProvider({ children }: PropsWithChildren<unknown>) {
+  return (
+    <TanstackQueryProvider>
+      <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
+        <ToastProvider />
+        {children}
+      </ThemeProvider>
+    </TanstackQueryProvider>
+  )
+}
